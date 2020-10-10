63c10e81bcdbc8ecc40e42a88e4d9fbf04b9e461
Kavindu <rakhranasinghe@gmail.com>
2020-10-10 18:18:50 +0530

    Fody added

---
+++ Safety_app/Safety_app/Models/Token.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+
+namespace Safety_app.Models
+{
+   public class Token
+    {
+        public int id;
+        public string access_token;
+        public string error_description;
+        public DateTime expire_date;
+        public Token()
+        {
+
+        }
+    }
+}
---
+++ Safety_app/Safety_app/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using System.Text;
+
+
+
+namespace Safety_app.Models
+{
+
+    public class User : INotifyPropertyChanged
+    {
+        public string username { get; set; }
+    public string password { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
