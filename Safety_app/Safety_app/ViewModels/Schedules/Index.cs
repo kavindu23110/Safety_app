1f4d6b7ed2d0f6c96bbaf0ee04e35f76ee3fea8b
Kavindu <rakhranasinghe@gmail.com>
2020-10-15 21:43:07 +0530

    Remove unnecessary classes

--- Safety_app/Safety_app/ViewModels/Schedules/Index.cs
+++ Safety_app/Safety_app/ViewModels/Schedules/Index.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Safety_app.ViewModels.Schedules
 {
   public  class Index:BaseViewModel
     {
-        internal Task LoadSchedulesAsync()
+        public ICommand SelectScheduleCommand { get; set; }
+        public ObservableCollection<Models.Shedule> lstSchedules { get; set; }
+
+        public Index()
         {
-            throw new NotImplementedException();
+            SelectScheduleCommand = new Command(OnSelectSchedule);
+        }
+
+        private void OnSelectSchedule(object obj)
+        {
+          
+        }
+
+        public async Task LoadSchedulesAsync()
+        {
+         var lst =await  App.Database.GetScheduleOperator().FindAsync(p => p.IsActive == 1);
+            lstSchedules = new ObservableCollection<Models.Shedule>(lst);
         }
     }
 }
