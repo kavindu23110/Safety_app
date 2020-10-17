3158e81e713e42de88dac459fba37d1a055bb876
Kavindu <rakhranasinghe@gmail.com>
2020-10-17 16:50:21 +0530

    Add dependancy service

---
+++ Safety_app/Safety_app/DependancyService/ITaskSchedular.cs
@@ -0,0 +1,13 @@
+﻿using Safety_app.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Safety_app.DependancyService
+{
+  public  interface ITaskSchedular
+    {
+        void TimerAddSchedule(Shedule shedule);
+        void TimerDeactivateSchedule(Shedule shedule);
+    }
+}
--- Safety_app/Safety_app/Helpers/StaticFunctions.cs
+++ Safety_app/Safety_app/Helpers/StaticFunctions.cs
@@ -1,14 +1,30 @@
-﻿using System;
+﻿using Safety_app.DependancyService;
+using Safety_app.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
 
 namespace Safety_app.Helpers
 {
-  public static  class StaticFunctions
+    public static class StaticFunctions
     {
-        public static async System.Threading.Tasks.Task<bool> DisplayAlert_DeleteAsync() {
+        public static async System.Threading.Tasks.Task<bool> DisplayAlert_DeleteAsync()
+        {
             return await Application.Current.MainPage.DisplayAlert("Reminder", "This Action Will Delete Item Permanently", "OK", "cancel");
         }
+        public static void ActivateSchedule(Shedule shedule)
+        {
+            ITaskSchedular taskSchedular = DependencyService.Get<ITaskSchedular>();
+            taskSchedular.TimerAddSchedule(shedule);
+
+        }
+        public static void DeActivateSchedule(Shedule shedule)
+        {
+            ITaskSchedular taskSchedular = DependencyService.Get<ITaskSchedular>();
+            taskSchedular.TimerDeactivateSchedule(shedule);
+        }
+
+
     }
 }
